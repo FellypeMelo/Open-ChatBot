d4c16113aeb49710685f4b8995fcc9c3362aa913
Fellype Melo <fellypesamuel1@gmail.com>
2026-05-07 07:37:17 -0300

    chore: initial project structure and config

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    LLAMA_SERVER_URL: str = "http://localhost:8080"
+    DATABASE_URL: str = "sqlite:///./chatbot.db"
+    MODEL_PATH: str = "models/model.gguf"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()

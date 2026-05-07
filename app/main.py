9d2cc45124fecd8983d4b65f74ab6e24dff1c12e
Fellype Melo <fellypesamuel1@gmail.com>
2026-05-07 08:33:29 -0300

    feat: add web ui and static file mounting

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from app.api import chat
+import os
 
 app = FastAPI(title="Open-ChatBot")
 
 app.include_router(chat.router)
 
+# Mount static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 async def root():
-    return {"message": "Open-ChatBot API is running"}
+    return FileResponse("static/index.html")
